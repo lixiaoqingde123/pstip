f1bf26f00b48cc1b9db9431c1fb6e4fa2807fbd3
cgzero <381247192@qq.com>
2013-08-21 15:43:51 +0800

    修改配置

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 edp build -f
 cleancss -o ./output/pstip.css ./src/pstip.css
+cleancss -o ./output/guarantee.css ./src/guarantee.css
+cleancss -o ./output/dropdownmenu.css ./src/dropdownmenu.css
